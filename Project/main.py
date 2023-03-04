1cb37ea2383ae5e93962387b10c489283c0b2501
Leandro Kornelius <leobelko@gmail.com>
2023-03-04 18:11:45 -0300

    improve table view and allow teacher to select number of grades

--- Project/main.py
+++ Project/main.py
@@ -1,20 +1,20 @@
-database = {}
-
 '''
 Students are allowed to consult their grades in classes their names are present.
 In this sense, to research information, the program must ask for their name to seek from the dicionaries
 '''
 
 def line_separator(biggest_string_len, number_of_columns):
-    separator = "*"
+    separator = "*" + ("-" * (biggest_string_len + 2)) + "*"
     for i in range(number_of_columns):
         separator += ("-" * (biggest_string_len + 2)) + "*"
     return separator
 
 def line_print(biggest_string_len, number_of_columns, line_header, grades):
     line = f'| {line_header.center(biggest_string_len)} |'
-    for i in range(number_of_columns):
+    for i in range(len(grades)):
         line += f' {grades[i].center(biggest_string_len)} |'
+    for i in range(number_of_columns - len(grades)):
+        line += f' {"Not Used".center(biggest_string_len)} |'
     return line
 
 
@@ -26,12 +26,12 @@ def student_search(student_name):
 
     # Student information search and format adaptations
     for subject in database:
-        if student_name in database[subject][2].keys():
+        if student_name in database[subject][3].keys():
             student_subjects.append(subject)
             if len(subject) > biggest_string:
                 biggest_string = len(subject)
-            if len(database[subject][2][student_name]) > biggest_columns:
-                biggest_columns = len(database[subject][2][student_name])
+            if len(database[subject][3][student_name]) > biggest_columns:
+                biggest_columns = len(database[subject][3][student_name])
     header_grades = []
     for i in range(1, biggest_columns + 1):
         header_grades.append(f'Grade {i}')
@@ -44,9 +44,11 @@ def student_search(student_name):
         separator = line_separator(biggest_string, biggest_columns)
         print(separator)
         print(line_print(biggest_string, biggest_columns, "Subject Name", header_grades))
+        print(separator)
         for student_subject in sorted(student_subjects):
-            grades = database[student_subject][2][student_name]
-            line_print(biggest_string, biggest_columns, student_subject, grades)
+            grades = database[student_subject][3][student_name]
+            print(line_print(biggest_string, biggest_columns, student_subject, grades))
+            print(separator)
 
 
     else: # Student not registered in any classes
@@ -130,7 +132,7 @@ def teacher_login():
         teacher_name = input("Please type your name: ")
         if not name_check(teacher_name): # checks if name is in database
             teacher_subject, teacher_subject_info = teacher_info(teacher_name)
-            teacher_password = input("Please register a password: ")
+            teacher_password = input("Please type your password: ")
             if teacher_password == teacher_subject_info[1]:
                 login_cond = False
             else: # incorrect password
@@ -162,36 +164,42 @@ def teacher_commands(teacher_subject):
               "4 - List students grades\n"
               "5 - End session")
         option = input("Which command would you like to perform: ")
-
+        # 1 - Add student
         if option == "1":
             student_name = input("What is the student name you would like to add? ")
             if student_name not in database[teacher_subject][3]:
                 database[teacher_subject][3][student_name] = database[teacher_subject][2] * ["-"]
             else:
                 print("There is already a student in your class with the name typed. Try again...")
-
+        # 2- Update student grade
         elif option == "2":
             student_name = input("What is the student's name you would like to alter grades? ")
             if student_name in database[teacher_subject][3]:
                 print(f'Here are {student_name} grades so far:')
                 grades = database[teacher_subject][3][student_name]
                 for i in range(1, len(grades) + 1):
-                    print(f'Grade {i} - {grades[i] - 1}')
+                    print(f'Grade {i}: {grades[i - 1]}')
                 update_which = input("Which one would you like to alter? ")
-                if update_which in range(1, len(grades)):
-                    update_value = input("What is the updated value (between 0 and 10)? ")
-                    if update_value.isalpha() == False:
-                        if 10 >= float(update_value) >= 0:
-                            database[teacher_subject][3][student_name][int(update_which) - 1] = update_value
+                if not update_which.isalpha():
+                    if update_which.isdigit():
+                        if int(update_which) in range(1, len(grades) + 1):
+                            update_value = input("What is the updated value (between 0 and 10)? ")
+                            if not update_value.isalpha():
+                                if 10 >= float(update_value) >= 0:
+                                    database[teacher_subject][3][student_name][int(update_which) - 1] = update_value
+                                else:
+                                    print("The typed number isn't in the interval defined. Try again...")
+                            else:
+                                print("String typed isn't a number. Try again...")
                         else:
-                            print("The typed number isn't in the interval defined. Try again...")
+                            print("Invalid option. Try again...")
                     else:
-                        print("String typed isn't a number. Try again...")
+                        print("Please type a whole number. Try again...")
                 else:
-                    print("Invalid option. Try again...")
+                    print("Please type a number. Try again...")
             else:
                 print("There isn't a student in your class with the name typed. Try again...")
-
+        # 3 - Search student
         elif option == "3":
             student_name = input("Which student would you like to search grades? ")
             if student_name in database[teacher_subject][3]:
@@ -209,71 +217,79 @@ def teacher_commands(teacher_subject):
                 print(line_print(biggest_string, biggest_columns, "Student Name", header_grades))
                 print(separator)
                 print(line_print(biggest_string, biggest_columns, student_name, grades))
+                print(separator)
             else:
                 print("There isn't a student in your class with the name typed. Try again...")
-
+        # 4 - List students grades
         elif option == "4":
             students = database[teacher_subject][3]
             biggest_string = len("Student Name")
+            biggest_columns = database[teacher_subject][2]
+            header_grades = []
+            for i in range(1, biggest_columns + 1):
+                header_grades.append(f'Grade {i}')
             for student in students:
                 if len(student) > biggest_string:
                     biggest_string = len(student)
-            sep = "*" + ("-" * (biggest_string + 2)) + "*" \
-                  + ("-" * (biggest_string + 2)) + "*" \
-                  + ("-" * (biggest_string + 2)) + "*" \
-                  + ("-" * (biggest_string + 2)) + "*"
-            print(sep)
-            line_print(biggest_string, "Subject Name", " Grade 1 ", " Grade 2 ", " Grade 3 ") # Header line
+            separator = line_separator(biggest_string, biggest_columns)
+            print(separator)
+            print(line_print(biggest_string, biggest_columns, "Student Name", header_grades))
+            print(separator)
             for student in sorted(students):
                 grades = database[teacher_subject][3][student]
-                line_print(biggest_string, student, grades[0], grades[1], grades[2])
-
+                print(line_print(biggest_string, biggest_columns, student, grades))
+                print(separator)
+        # 5 - End teacher command session
         elif option == "5":
             cont_cond = False
         else:
             print("Please type a valid option. Try again...")
 
 
-def teacher_access():
-    access_cond = True
-    while access_cond:
-        if input("Type the teacher access password provided by the program: ") == "teacher":
-            access_cond = False
-        else:
-            print("The typed access password is incorrect.")
-            student = input("Would you like to return to the main page?(yes/no) ")
-            if student.lower() == "yes":
+main_cond = True
+print("Welcome to the school grading system!")
+
+# Loading content from txt file
+f = open("content.txt", "r")
+for line in f:
+    database = eval(line.strip())
+f.close()
+
+while main_cond:
+    persona = input("Are you a student or teacher? ")
+    if persona == "teacher":
+        access_cond = True
+        while access_cond:
+            if input("Type the teacher access password provided by the program: ") == "teacher":
                 access_cond = False
-                main()
+            else:
+                print("The typed access password is incorrect.")
+                student = input("Would you like to return to the main page?(yes/no) ")
+                if student.lower() == "yes":
+                    access_cond = False
+        account_cond = True
+        while account_cond:
+            signed = input("Are you already signed up?(yes/no) ")
+            if signed.lower() == "yes":
+                teacher_name, teacher_subject, teacher_subject_info = teacher_login()
+                account_cond = False
+            elif signed.lower() == "no":
+                teacher_name, teacher_subject, teacher_subject_info = teacher_signup()
+                account_cond = False
+            else:
+                print("Invalid option. Try again...")
+        teacher_commands(teacher_subject)
+        print(f'Thank you, {teacher_name}! Come back anytime...')
+    elif persona == "student":
+        student_search(input("Type your name to do the search: "))
+    else:
+        print("Please type student or teacher. Try again...")
+    keep_going = input("Would you like to keep going?(yes/no) ")
+    if keep_going.lower() == "no":
+        main_cond = False
+print("Logging off...")
 
-
-def main():
-    main_cond = True
-    print("Welcome to the school grading system!")
-    while main_cond:
-        persona = input("Are you a student or teacher? ")
-        if persona == "teacher":
-            teacher_access()
-            account_cond = True
-            while account_cond:
-                signed = input("Are you already signed up?(yes/no) ")
-                if signed.lower() == "yes":
-                    teacher_name, teacher_subject, teacher_subject_info = teacher_login()
-                    account_cond = False
-                elif signed.lower() == "no":
-                    teacher_name, teacher_subject, teacher_subject_info = teacher_signup()
-                    account_cond = False
-                else:
-                    print("Invalid option. Try again...")
-            teacher_commands(teacher_subject)
-            print(f'Thank you, {teacher_name}! Come back anytime...')
-        elif persona == "student":
-            student_search(input("Type your name to do the search: "))
-        else:
-            print("Please type student or teacher. Try again...")
-        keep_going = input("Would you like to keep going?(yes/no) ")
-        if keep_going.lower() == "no":
-            main_cond = False
-    print("Logging off...")
-
-main()
+# Saving content
+f = open("content.txt", "w")
+f.write(str(database))
+f.close()
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ The stack used to develop the student-grader was python.\
 Having in mind the desire to use dictionaries data structures to solidate my knowledges with this datatype, this program applies a simple grading system to allow teachers to register classes, add students grades, and view information.\
 Also, it let's students view all their grades for the classes they belong.\
 Some possible improvements can be:\
-1 - Have a database to save the information from previous uses\
+1 - Implement a not only local database to store class informations\
 2 - Develop a GUI to improve UI/UX\
-3 - Allow the teacher to configurate their grading system, number of grades, and weight of each test
+3 - Allow the teacher to further configurate their grading system, not only by number but weight
